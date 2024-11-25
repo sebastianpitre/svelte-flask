91f059bd17adb9020f40bcc56692cc89634ef8b2
sebastianpitre <sebastianandrespitrediaz@gmail.com>
2024-11-25 12:04:06 -0500

    cambios alisas

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'dist',  // Carpeta donde se guardará la construcción de producción
+    sourcemap: true,  // Genera archivos sourcemap para depuración
+    minify: 'esbuild',  // Usa esbuild para la minificación, que es muy rápido
+    chunkSizeWarningLimit: 500,  // Ajusta el límite de tamaño para los "chunks"
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',  // Alias para la carpeta de componentes
+      '@utils': '/src/utils',  // Alias para la carpeta de utilidades (si tienes alguna)
+    },
+  },
 })
