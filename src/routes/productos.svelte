ec5aba690d18440f754bf0af08709d505745a60e
sebastianpitre <sebastianandrespitrediaz@gmail.com>
2024-12-08 18:32:22 -0500

    salidas

--- src/routes/productos.svelte
+++ src/routes/productos.svelte
@@ -153,7 +153,7 @@
                                     <button class="btn mx-1 col-auto btn-sm btn-outline-danger" on:click={() => eliminarProducto(values.id)}>Eliminar</button>
                                     <a class="btn col-auto mx-1 btn-sm btn-outline-success" href={`/producto/${values.id}`}>Editar</a>
                                     <a class="btn col-auto mx-1 btn-sm btn-outline-dark" href={`/entrada/${values.id}`}>Entrada →</a>
-                                    {#if values.stock <= 0}
+                                    {#if values.stock > 0}
                                     <a class="btn col-auto mx-1 btn-sm btn-outline-danger" href={`/salida/${values.id}`}>Salida →</a>
                                     {:else}
                                     <div class="btn col-auto mx-1 btn-sm btn-outline-danger disabled">Salida →</div>
